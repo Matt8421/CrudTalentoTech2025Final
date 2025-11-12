f2cc20d1251bd1f2a2419cbd89ce4287b5e4cbe8
Matt <matiaslucero21884@gmail.com>
2025-11-12 20:35:22 -0300

    Agregando controller

--- src/main/java/com/talento/crud/controller/ArticuloController.java
+++ src/main/java/com/talento/crud/controller/ArticuloController.java
@@ -1,5 +1,25 @@
+//Paquete del controlador
 package com.talento.crud.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.talento.crud.service.ArticuloService;
+
+//Controlador REST que maneja todas las peticiones HTTP - GET(id), POST, PUT, DELETE, - PATCH - OPTIONS
+@RestController
+//@RequestMapping define la ruta base para todas las peticiones de este controlador
+//En este caso, todas las peticiones a este controlador comenzaran con api/articulos
+//http://localhost:8080/api/articulos
+
+@RequestMapping("api/articulos")
 public class ArticuloController {
-    
+
+    //Inyeccion del servicio
+    private final ArticuloService articuloService;
+
+    //Constructor con inyeccion
+    //para que Spring pueda inyectar el servicio automaticamente
+    //cdo se crea una instancia de este controlador
+    //Esto permite seguir el circuito de inversion de dependencias
 }
