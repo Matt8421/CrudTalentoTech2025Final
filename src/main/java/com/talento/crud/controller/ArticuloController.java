297eb0cd71fb4828738e89157528bf3db15693f5
Matt <matiaslucero21884@gmail.com>
2025-11-12 21:52:22 -0300

    Modificando Controller

--- src/main/java/com/talento/crud/controller/ArticuloController.java
+++ src/main/java/com/talento/crud/controller/ArticuloController.java
@@ -3,11 +3,16 @@ package com.talento.crud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.talento.crud.model.Articulo;
 import com.talento.crud.service.ArticuloService;
 
 //Controlador REST que maneja todas las peticiones HTTP - GET(id), POST, PUT, DELETE, - PATCH - OPTIONS
@@ -72,8 +77,44 @@ public class ArticuloController {
         return articuloService.obtenerArticuloPorId(id)
             .map(ResponseEntity::ok)   //Devuelve 200  si lo encuentra
             .orElse(ResponseEntity.notFound().build());   //Devuelve 404 sino 
-
     }
     
+    //POST /api/articulos -> crea un nuevo articulo
+    @PostMapping
+    //@RequestBody indica q el cuerpo de la peticion contiene los datos del articulo
+    //que se va a crear.Spring convierte automaticamente el json del cuerpo en un objeto Articulo
+    //El metodo crear recibe un obj articulo y lo guarda usando el servicio.
+    public Articulo crear(@RequestBody Articulo articulo){
+        return articuloService.guardarArticulo(articulo);
+    }
+
+    //PUT /api/articulos/{id}  -> actualiza un articulo existente
+    @PutMapping("/{id}"){
+        public ResponseEntity<Articulo> actualizar(@PathVariable Long id, @RequestBody Articulo articulo) {
+            if (articuloService.obtenerArticuloPorId(id).isEmpty()) {
+                return ResponseEntity.notFound().build();  //SIno existe
+            }
+            return ResponseEntity.ok(articuloService.actualizarArticulo(id, articulo));  //SI existe , actualiza y 200 
+        
+    }
+    //DELETE /api/articulos/{id} -> elimina un articulo
+    @DeleteMapping("/{id}")
+    //Elimina un articulo por ID.
+    //Si el articulo no existe , devuelve un 404 Not Found.
+    //Si se elimina correctamente devuelve un 204 No Content.
+    //El metodo eliminar recibe el id del metodo eliminar.
+    //Si el articulo no existe , devuelve un ResponseEntity con un codigo 404 Not Found.
+    //Si el articulo existe , lo elimina y devuelve un ResponseEntity con un codigo 204 No Content.
+    //Esto es util para APIs REST donde es comun eliminar recursos.
+    //XQ un 204 No Content? EL 204 indica q la solicitud se ha procesado correctamente.,
+    //pero no hay contenido q devolver en la respuesta.
+    //Es una forma de indicar q la operacion se completo con exito.
+    public ResponseEntity<Void> eliminar (@PathVariable Long id) {
+        if (articuloService.obtenerArticuloPorId(id).isEmpty()) {
+            return ResponseEntity.notFound().build();  //Sino existe, 404
+        }
+        articuloService.eliminarArticulo(id);
+        return ResponseEntity.noContent().build();  //204 No content
+    }
 
 }
